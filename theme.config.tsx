b2dd18ef98007d27d4264837cd5454911f2da038
antuntune <antekljaj@gmail.com>
2025-01-09 22:34:20 +0100

    danPrvi

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,11 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
+  logo: <span>Upute za montažu</span>,
+  
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Đenka Photobooth',
   },
 }
 
